1cf7aba3b67fe74d2a101a8bbed811afba72e076
amercader <amercadero@gmail.com>
2021-01-08 12:53:59 +0100

    Fallback to a giftless.yaml file if GIFTLESS_CONFIG_FILE is not set
    Only when running a local development server with Flask

--- flask-develop.sh
+++ flask-develop.sh
@@ -4,4 +4,7 @@ export FLASK_ENV=development
 export FLASK_APP=giftless.wsgi_entrypoint
 export GIFTLESS_DEBUG=1
 
+export GIFTLESS_CONFIG_FILE=${GIFTLESS_CONFIG_FILE:-giftless.yaml}
+echo "Using configuration file: $GIFTLESS_CONFIG_FILE"
+
 flask run $@
