d75f54511db003481ca85d7a6ce30385399ec68e
Tomasz Sabała <tomek@fjelltopp.org>
2021-03-09 10:59:12 +0100

    Get download and upload url implementations.

--- giftless/storage/aws_s3.py
+++ giftless/storage/aws_s3.py
@@ -1,6 +1,6 @@
-import io
 import logging
 import os
+import urllib
 from collections import namedtuple
 from typing import Any, BinaryIO, Dict, Iterable, List, Optional
 
@@ -19,6 +19,7 @@ _log = logging.getLogger(__name__)
 class AwsS3Storage(StreamingStorage, ExternalStorage, MultipartStorage):
     """AWS S3 Blob Storage backend.
     """
+
     def __init__(self, aws_access_key_id: str, aws_secret_access_key: str,
                  aws_s3_bucket_name: str, path_prefix: str):
         self.aws_access_key_id = aws_access_key_id
@@ -26,12 +27,13 @@ class AwsS3Storage(StreamingStorage, ExternalStorage, MultipartStorage):
         self.aws_s3_bucket_name = aws_s3_bucket_name
         self.path_prefix = path_prefix
         self.s3: boto3.session.Session.resource = boto3.resource('s3')
+        self.s3_client = boto3.client('s3')
 
     def get(self, prefix: str, oid: str) -> Iterable[bytes]:
         if not self.exists(prefix, oid):
             raise ObjectNotFound()
-        return self._s3_object(prefix,oid).get()['Body']
-    
+        return self._s3_object(prefix, oid).get()['Body']
+
     def put(self, prefix: str, oid: str, data_stream: BinaryIO) -> int:
         bucket = self.s3.Bucket(self.aws_s3_bucket_name)
         res = bucket.put_object(Key=self._get_blob_path(prefix, oid), Body=data_stream)
@@ -56,11 +58,47 @@ class AwsS3Storage(StreamingStorage, ExternalStorage, MultipartStorage):
 
     def get_upload_action(self, prefix: str, oid: str, size: int, expires_in: int,
                           extra: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-        return
+        # TODO: The current approach with urlencoding fields doesn't work
+        # AWS returns "The specified method is not allowed against this resource." error
+        response = self.s3_client.generate_presigned_post(self.aws_s3_bucket_name,
+                                                          self._get_blob_path(prefix, oid),
+                                                          ExpiresIn=expires_in,
+                                                          **extra
+                                                          )
+        params = urllib.parse.urlencode(response['fields'])
+        href = f"{response['url']}?{params}"
+        return {
+            "actions": {
+                "upload": {
+                    "href": href,
+                    "header": {},
+                    "expires_in": expires_in
+                }
+            }
+        }
 
     def get_download_action(self, prefix: str, oid: str, size: int, expires_in: int,
                             extra: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
-        return
+
+        filename = extra.get('filename') if extra else oid
+        params_ = {
+            'Bucket': self.aws_s3_bucket_name,
+            'Key': self._get_blob_path(prefix, oid),
+            'ResponseContentDisposition': f"attachment; filename = {filename}"
+        }
+        response = self.s3_client.generate_presigned_url('get_object',
+                                                         Params=params_,
+                                                         ExpiresIn=expires_in
+                                                         )
+        return {
+            "actions": {
+                "download": {
+                    "href": response,
+                    "header": {},
+                    "expires_in": expires_in
+                }
+            }
+        }
 
     def get_multipart_actions(self, prefix: str, oid: str, size: int, part_size: int, expires_in: int,
                               extra: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
